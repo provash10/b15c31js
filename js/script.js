ed422797c02298c5d2ef710520ab5c81508ce524
provash10 <provash20cb@gmail.com>
2024-11-26 01:19:40 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let marks = [56, 61, 90, 30, 58, 99];
+
+for(let i=0; i<=5; i++){
+    console.log(marks[i]);
+}
+
+for(let i=1; i<=10; i++){
+    console.log("hello world");
+}
+
+marks.forEach((element)=>{
+    console.log(element);
+})
+
+// While Loop
+
+let i=0;
+while(i<=5){
+    console.log(marks[i]);
+}
